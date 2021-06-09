4925088fc5f23342a6820ef5d1ffe8415d15ad32
Yurii Lisovych <yurii.lisovych.se@gmail.com>
2021-06-09 10:42:11 +0300

    add error handling

--- script.js
+++ script.js
@@ -12,22 +12,27 @@ console.log(secondNumber, typeof secondNumber);
 
 let sum = 0;
 
-const skip = confirm("Skip even numbers?")
-
-if(skip === true){
-    console.log("Skip even");
-    for (let i = firstNumber; i <= secondNumber; i++){
-    if (i % 2 === 0) {
-        continue;
-    }
-        sum = sum + i;
-    }
-    console.log("skip even sum " + sum);
-    alert("skip even sum " + sum);
+if (firstNumber > secondNumber){
+    console.log("Error: first number is bigger then second number");
+    alert("Error: first number is bigger then second number");
 } else {
-    for (let i = firstNumber; i <=secondNumber; i++){
-        sum = sum + i;
+    const skip = confirm("Skip even numbers?")
+
+    if(skip === true){
+        console.log("Skip even");
+        for (let i = firstNumber; i <= secondNumber; i++){
+            if (i % 2 === 0) {
+                continue;
+            }
+            sum = sum + i;
+        }
+        console.log("skip even sum " + sum);
+        alert("skip even sum " + sum);
+    } else {
+        for (let i = firstNumber; i <=secondNumber; i++){
+            sum = sum + i;
+        }
+        console.log("Sum of numbers " + sum);
+        alert("Sum of numbers " + sum);
     }
-    console.log("Sum of numbers " + sum);
-    alert("Sum of numbers " + sum);
 }
